ee23488a6d8e327bdf5d1650feb2f122ca48086f
yeweigao <64359184+yeweigao@users.noreply.github.com>
2024-05-22 15:19:36 +0800

    增加4o

--- main.ts
+++ main.ts
@@ -10,6 +10,7 @@ const mapper:any = {
   'gpt-3.5-turbo': Deno.env.get("DEPLOY_NAME_GPT35"),
   'gpt-3.5-turbo-16k': Deno.env.get("DEPLOY_NAME_GPT35_16k"),
   'gpt-4': Deno.env.get("DEPLOY_NAME_GPT4"),
+  'gpt-4o': Deno.env.get("DEPLOY_NAME_GPT4o"),
   'gpt-4-32k': Deno.env.get("DEPLOY_NAME_GPT4_32k"),
   // Other mapping rules can be added here.
 };
